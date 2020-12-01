d32a8d52c0a7c09868899df97b20b6912b081813
Roel Peters <roel@p-tmc.com>
2020-12-01 21:03:30 +0100

    added selected_fields argument

--- sett/main.py
+++ sett/main.py
@@ -30,7 +30,7 @@ def Sett(request):
 
   bq_client = bigquery.Client.from_service_account_json('creds.json')
   table_id = 'roelpeters-blog.web.badgr_lake'
-  errors = bq_client.insert_rows(table_id, [{'event',json.dumps(badgr)}])
+  errors = bq_client.insert_rows(table = table_id, rows = [{'event',json.dumps(badgr)}], selected_fields = ['event'])
 
   if errors == []:
       print("New rows have been added.")
